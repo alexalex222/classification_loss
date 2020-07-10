0e384deb0f67a17e900280c8b4af4ac845e0ab77
Kuilin Chen <alexchen199023@gmail.com>
2020-07-10 10:25:54 -0400

    crate prepare data

---
+++ prepare_data.py
@@ -0,0 +1,20 @@
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+
+
+def get_mnist_data(data_dir='C:\\Temp\\torch_dataset', train_batch_size=64, test_batch_size=1000, workers=4):
+    train_loader = DataLoader(
+        datasets.MNIST(data_dir, train=True, download=False,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=train_batch_size, shuffle=True, num_workers=workers)
+    test_loader = DataLoader(
+        datasets.MNIST(data_dir, train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=test_batch_size, shuffle=False, num_workers=workers)
+
+    return train_loader, test_loader
